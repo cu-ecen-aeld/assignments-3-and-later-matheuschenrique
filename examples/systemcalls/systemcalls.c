6034d368885755cd012604e0359005a381cfa819
matheuschenrique <matheuschenrique1@gmail.com>
2024-09-22 00:35:09 -0300

    fix do_system return

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -21,7 +21,7 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-    return system(cmd) >= 0;
+    return system(cmd) == 0;
 }
 
 /**
